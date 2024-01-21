a07bcd025014e5dc39a0bf8ceb1c14630a71e4f2
DuHyeonChoi <engus9902@gmail.com>
2024-01-21 18:15:31 +0900

    Feat(App.jsx): 라우터 추가
    signin, signup

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<div>signin</div>} />
+        <Route path="/signup" element={<div>signup</div>} />
       </Routes>
     </BrowserRouter>
   );
