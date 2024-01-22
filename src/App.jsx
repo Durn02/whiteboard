664f3314fb4e70caf13bb39b0c20c81ea5525b98
DuHyeonChoi <engus9902@gmail.com>
2024-01-22 21:54:39 +0900

    Feat(App.jsx): 로그인 페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import SigninPage from "./pages/SigninPage/SigninPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/signin" element={<div>signin</div>} />
+        <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<div>signup</div>} />
       </Routes>
     </BrowserRouter>
