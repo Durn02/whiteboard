f05333d784ed026579eabdb57c3d7cb2c242983a
DuHyeonChoi <engus9902@gmail.com>
2024-01-23 01:24:30 +0900

    Docs: placeHolder -> placeholder로 수정

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 import styles from "./Button.module.css";
 
-const Button = ({ placeHolder, as, to }) => {
+const Button = ({ placeholder, as, to }) => {
   if (as === "Link") {
     return (
       <Link to={to}>
-        <button className={styles.button}>{placeHolder}</button>
+        <button className={styles.button}>{placeholder}</button>
       </Link>
     );
   }
-  return <button className={styles.button}>{placeHolder}</button>;
+  return <button className={styles.button}>{placeholder}</button>;
 };
 
 export default Button;
