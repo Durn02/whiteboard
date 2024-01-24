49cd57163808d480ef1f14499550a172ea72cee5
DuHyeonChoi <engus9902@gmail.com>
2024-01-25 00:43:25 +0900

    Feat(SignupPage): 회원가입 페이지 생성

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,5 +1,26 @@
+import style from "./SignupPage.module.css";
+import { Link } from "react-router-dom";
+import Input from "../../components/Input/Input/Input";
+import PwInput from "../../components/Input/PwInput/PwInput";
+import Button from "../../components/Button/Button";
+import Select from "../../components/Select/Select";
+
 const SignupPage = () => {
-  return <>회원가입페이지</>;
+  return (
+    <div className={style.container}>
+      <div className={style.signinbox}>
+        <div className={style.title}>
+          <Link to={"/"}>WhiteBoard</Link>
+        </div>
+        <Input placeholder={"아이디"} />
+        <PwInput placeholder={"비밀번호"} />
+        <Input placeholder={"이름"} />
+        <Input placeholder={"학번"} />
+        <Select />
+        <Button placeholder={"회원가입"} />
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
